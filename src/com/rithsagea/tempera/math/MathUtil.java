5e0e9d3b3ed71a2a3128723f529ee7f6eb88cd65
Rithsagea Aquadom <rithsagea@gmail.com>
2021-11-05 02:35:31 -0400

    also useful

--- src/com/rithsagea/tempera/math/MathUtil.java
+++ src/com/rithsagea/tempera/math/MathUtil.java
@@ -4,4 +4,11 @@ public class MathUtil {
 	public static boolean within(int val, int low, int high) {
 		return val >= low && val <= high;
 	}
+	
+	public static double clamp(double val, double low, double high) {
+		if(val < low) return low;
+		if(val > high) return high;
+		
+		return val;
+	}
 }
